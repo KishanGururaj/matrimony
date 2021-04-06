658dc53818b04732e8ba0dc9aed2995c907c3d45
Kishan G <kishan@wolkensoftware.com>
2021-04-06 23:17:45 +0530

    Adding:matrimony using spring mvc

---
+++ src/main/java/com/wolken/wolkenapp/exception/CandidatesException.java
@@ -0,0 +1,10 @@
+package com.wolken.wolkenapp.exception;
+
+public class CandidatesException  extends Exception{
+	@Override
+	public String getMessage() {
+		// TODO Auto-generated method stub
+		return "Enter into custom exception inside validate and save";
+	}
+
+}
